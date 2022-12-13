c2c69ffe75a2e36812e6f2515f2d85533c153da1
Alexandr <Bioreflector@gmail.com>
2022-12-13 12:26:11 +0200

    created config file with url

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const API_KEY = '?api_key=0173d6afde5e852201a2850602f70181'
+const URL_GANRES = 'https://api.themoviedb.org/3/genre/movie/list'
+const URL_DEFAULT = 'https://api.themoviedb.org/3/'
+const URL_IMAGES = 'https://image.tmdb.org/t/p/w500/'
