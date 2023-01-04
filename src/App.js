eb938b2ae699517ae3dbc3e42151d73d31df070c
Alexandr <Bioreflector@gmail.com>
2023-01-04 13:08:15 +0200

    rebuild movie page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useEffect } from 'react'
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { URL_DEFAULT, URL_GANRES, API_KEY } from './config'
 import Context from './Context'
 import MainLayout from './layout/MainLayout'
 import MoviesPage from './components/MoviesPage'
@@ -10,40 +8,18 @@ import AboutMovie from './components/AboutMovie'
 import './App.css'
 
 function App() {
-  const [genres, setGanres] = useState([])
-  const [movies, setMovies] = useState([])
   const [watchList, setWatchList] = useState([])
-  useEffect(() => {
-    fetch(`${URL_GANRES}${API_KEY}`).then((response) =>
-      response.json().then((genres) => setGanres(genres.genres))
-    )
-  }, [])
-  useEffect(() => {
-    fetch(`${URL_DEFAULT}/movie/top_rated${API_KEY}`).then((response) =>
-      response.json().then((movie) => setMovies(movie))
-    )
-  }, [])
   const value = {
     watchList,
-    setWatchList
+    setWatchList,
   }
-
   return (
     <Context.Provider value={value}>
       <BrowserRouter>
         <div className="App">
           <Routes>
             <Route path="/" element={<MainLayout />}>
-              <Route
-                index
-                element={
-                  <MoviesPage
-                    movies={movies}
-                    setMovies={setMovies}
-                    genres={genres}
-                  />
-                }
-              />
+              <Route index element={<MoviesPage />} />
               <Route path="/:id" element={<AboutMovie />} />
               <Route path="watchlist" element={<WatchList />} />
             </Route>
--- src/components/MoviesPage.jsx
+++ src/components/MoviesPage.jsx
@@ -1,8 +1,22 @@
+import { useEffect , useState } from 'react'
+import { URL_DEFAULT, URL_GANRES, API_KEY } from '../config'
 import Movie from './Movie'
 import SearchMovie from './SearchMovie'
 
-function MoviesPage(props) {
-  const { movies, setMovies, genres } = props
+function MoviesPage() {
+  const [genres, setGanres] = useState([])
+  const [movies, setMovies] = useState([])
+  
+  useEffect(() => {
+    fetch(`${URL_GANRES}${API_KEY}`).then((response) =>
+      response.json().then((genres) => setGanres(genres.genres))
+    )
+  }, [])
+  useEffect(() => {
+    fetch(`${URL_DEFAULT}/movie/top_rated${API_KEY}`).then((response) =>
+      response.json().then((movie) => setMovies(movie))
+    )
+  }, [])
   const { results } = movies
   return (
     <div className="moviePage">
